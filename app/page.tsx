35d587469332040cfcd6c3a5acf792ce1fc2b394
Cyrc3 <becerril23emilio@gmail.com>
2024-02-16 12:29:10 -0600

    ABER SI JALA

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import 'tailwindcss/tailwind.css';
+
 import styles from "@/styles/index.module.css"; // Cambio de 'Style' a 'styles'
 
 export default function Home() {
@@ -10,12 +12,17 @@ export default function Home() {
           <a href="#">About Us</a>
         </nav>
       </div>
-      <div>
-        <nav>
-          <a href="#">Hi, i'm Emilio</a>
-        
-        </nav>
-      </div>
+      <body className={styles.body}>
+        <h2>Hi, my name is Emilio uwu PART1</h2>
+      </body>
     </main>
   );
 }
+function MyComponent() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      Este es un componente con estilos de Tailwind CSS.
+    </div>
+  )
+}
+
